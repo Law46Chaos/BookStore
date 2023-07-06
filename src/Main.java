ce3a368f55242dfbd4fc581df88eda28104a8a2e
Law46Chaos <10parthan10@gmail.com>
2023-07-06 11:22:37 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+public static void main(String [] args) {
+	Scanner sc = new Scanner (System.in);
+	int ch;
+	System.out.println("Welcom to book store app");
+	System.out.println("========================");
+	do
+	{
+		System.out.println("1. Add Book");
+		System.out.println("2. View Book");
+		System.out.println("3. Update Book");
+		System.out.println("4. Delete Book");
+		System.out.println("5. Add Book");
+		ch=sc.nextInt();
+		switch(ch)
+		{
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			System.out.println("thank you for using our service");
+			break;
+			
+			}
+		}
+	}
+}
